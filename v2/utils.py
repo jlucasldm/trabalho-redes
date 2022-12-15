de78c13a85350e514dd0424fc1caf6cdbee92b09
João Lucas <jlucas.ldm@gmail.com>
2022-12-15 16:52:35 -0300

    limpando o repo

--- v2/utils.py
+++
@@ -1,6 +0,0 @@
-def message_formatter(msg, FORMAT, HEADER):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    return message, send_length
